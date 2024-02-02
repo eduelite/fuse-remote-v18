cf0d855f78e6ef81a834992972c3d91a449fd756
hustacdjm <hustacdjm@gmail.com>
2024-02-02 16:58:44 -0700

    change the reactive compnent to data

--- src/app/modules/test/test.component.ts
+++ src/app/modules/test/test.component.ts
@@ -1,13 +1,16 @@
 import {AfterViewInit, Component, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation} from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 import {FMComponent} from "../component/fm_registerId.component";
 
 @Component({
     selector     : 'test',
     standalone   : true,
     templateUrl  : './test.component.html',
-    encapsulation: ViewEncapsulation.None,
+    encapsulation: ViewEncapsulation.None,    
     imports:[
-        FMComponent
+        FMComponent,MatButtonModule, RouterLink, MatIconModule
     ]
 })
 export class TestComponent implements  OnInit 
@@ -16,32 +19,37 @@ export class TestComponent implements  OnInit
      * Constructor
      */
     @ViewChild('test', {read:ViewContainerRef}) testContainer!: ViewContainerRef;
-
-    fmComponentRef:any;
-    data:object={
-        content:{
-            question: 'this is the exmaple question?'
+  
+    reactiveComponent={
+        component:{
+          content:{},
+          ui:{},      
         },
-        comoponent:{
-
-        },
-        user:{
-            input:{},
-            score:{},
-            feedback:{}
+        runtime:{
+          data:{},
+          score:{},
+          feedback:{}
         }
-    };
-
+      };
+  
     constructor()
     {
 
 
     }
 
+    StartTest(){
+
+        console.log('container:' + this.testContainer);
+        const fmComponentRef  = this.testContainer!.createComponent(FMComponent);
+        (fmComponentRef as any).instance.data = this.reactiveComponent;
+        fmComponentRef.changeDetectorRef.markForCheck();
+
+    }
+
     ngOnInit(): void {
 
-        console.log(this.testContainer);
-        this.fmComponentRef = this.testContainer!.createComponent(FMComponent);
-        this.fmComponentRef.instance!.data = this.data;
+        
+        
     }
 }
