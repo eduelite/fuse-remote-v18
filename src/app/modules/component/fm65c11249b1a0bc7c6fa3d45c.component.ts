c2cc2f03a827414b00163d857aa0f36e4b1e2446
hustacdjm <hustacdjm@gmail.com>
2024-02-05 13:03:40 -0700

    ready for remote test

--- src/app/modules/component/fm65c11249b1a0bc7c6fa3d45c.component.ts
+++ src/app/modules/component/fm65c11249b1a0bc7c6fa3d45c.component.ts
@@ -51,7 +51,7 @@ export class FMComponent
                 { 
                     key: option.key,
                     value: option.value,
-                    isChecked:  this.data.runtime.data.answer && this.data.runtime.data.answer.includes (option.key)
+                    isChecked:  this.data.runtime.data.answer && this.data.runtime.data.answer.includes (option.key)===null?false:this.data.runtime.data.answer && this.data.runtime.data.answer.includes (option.key)
         
                 },
                 ...this.checkboxOptions
